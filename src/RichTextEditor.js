f325591d8143160c2a8e3271fcace746f56bb422
Anushavan1996 <anushavan.karapetyan@fluxtechs.com>
2018-10-11 16:14:33 +0400

    Changing props position to set "hideKeyboardAccessoryView" and "keyboardDisplayRequiresUserAction" from outside. Now if you don't want to show keyboard in first render, you can pass "keyboardDisplayRequiresUserAction={true}" from outside.

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -300,9 +300,9 @@ export default class RichTextEditor extends Component {
     return (
       <View style={rootStyle}>
         <WebViewBridge
-          {...this.props}
           hideKeyboardAccessoryView={true}
           keyboardDisplayRequiresUserAction={false}
+          {...this.props}
           ref={(r) => {this.webviewBridge = r}}
           onBridgeMessage={(message) => this.onBridgeMessage(message)}
           injectedJavaScript={injectScript}
